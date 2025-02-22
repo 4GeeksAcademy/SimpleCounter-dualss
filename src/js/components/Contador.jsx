365f5f94c8ed41964c4750b2f923925d94d5db36
dualss <dariongduals@gmail.com>
2025-02-22 09:22:22 +0000

    Contador simple

---
+++ src/js/components/Contador.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+const Contador = (props) =>{
+    
+    return (
+        <>
+        <div id="counter">
+            {
+                props.time
+            }
+        </div>
+            
+        </>
+    )
+
+}
+export default Contador;
--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,21 @@
 import React from "react";
+import Contador from "./Contador"
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
+let segundos = 456;
+function updateCounter(){
+	segundos++;
+	document.getElementById("counter").innerText = `${segundos}`;
+}
+setInterval(updateCounter, 1000);
 const Home = () => {
+	
 	return (
 		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+           <Contador time={segundos}></Contador> 
 		</div>
 	);
 };
